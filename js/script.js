a8f825727aa035eb5bb0055f2eb1a4f734ad0aee
italo jose <italo.alcantara@e-deploy.com.br>
2020-02-29 13:13:56 -0300

    brincando com js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*var resposta = confirm("Deseja excluir?");
+console.log(resposta);
+
+if (resposta==true) {
+    alert("Atualizando a pagina");
+    window.location.reload()
+}*/
+
+//var a=10,b=20;
+
+function somar(a,b){
+    return a + b;
+}
+
+console.log(somar(20,35))
